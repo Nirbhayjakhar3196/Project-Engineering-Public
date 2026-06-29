f409d7ddc121e1732a97c1d76775110689af4d4e
nirbhayjakhar3196 <jakharnirbhay0000@gmail.com>
2026-06-29 22:35:58 +0530

    fix: add role checks to all endpoints

--- Milestone 06/Auth Fire Drill/server/index.js
+++ Milestone 06/Auth Fire Drill/server/index.js
@@ -9,8 +9,26 @@ const PORT = 5001;
 
 // BROKEN PART 5: CSRF vulnerability (no protection)
 // CORS set to * (accepts requests from any origin)
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'http://localhost:5173' , credentials : true }));
 app.use(express.json());
+
+app.use((req , res , next) => {
+
+  if(["POST" , "PUT" , "DELETE"].includes(req.method)){
+
+    const csrfToken = req.header["x-csrf-token"];
+
+    if(csrfToken != secure-csf-token){
+      return res.status(403).json({
+        error: "Invalid CSRF Token"
+      })
+    }
+
+    next();
+
+  }
+
+})
 
 app.use('/api/auth', authRoutes);
 app.use('/api/fragments', fragmentRoutes);
